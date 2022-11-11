dfc438e0523e6afda2c537a1f157aa51dab7b397
Yuma Ono <yuma.ono@rakus-partners.co.jp>
2022-11-11 11:02:04 +0900

    ex1-2修正

--- src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
+++ src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
@@ -6,25 +6,10 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//	}
-//
-//	@AfterAll
-//	static void tearDownAfterClass() throws Exception {
-//	}
-//
-//	@BeforeEach
-//	void setUp() throws Exception {
-//	}
-//
-//	@AfterEach
-//	void tearDown() throws Exception {
-//	}
-
 	@Test
 	void test() {
-		int result = Calculator.divide(4,0);
+		Calculator calculator = new Calculator();
+		int result = calculator.divide(4,0);
 		assertEquals(0, result);
 	}
 
---
+++ src/test/java/junit/tutorial/ex01/e03/CounterTest.java
@@ -0,0 +1,14 @@
+package junit.tutorial.ex01.e03;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CounterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
